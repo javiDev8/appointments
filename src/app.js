0c3205cc1dcc68c78a389e6ac2f5a5d5293e45cc
javi <javidev.8@gmail.com>
2020-08-08 15:46:56 -0500

    static files
    set static files
    catch client side router requests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.get('/', (req, res) => res.send('testing'))
+// static files (bundles)
+app.use('/', express.static(path.resolve(__dirname, 'user/static')))
+app.use('/admin', express.static(path.resolve(__dirname, 'admin/static')))
+
+// catch client side router request
+app.get('/*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'user/static/index.html'))
+)
+app.get('/admin/*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'admin/static/index.html'))
+)
 
 const { port } = require('./env.js')
 app.listen(port, () => console.log('server on port: ', port))
