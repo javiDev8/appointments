88e1ab65cbaef584b96fc25c6e5207ae2ad9f43a
javi <javidev.8@gmail.com>
2020-08-13 18:41:35 -0500

    get available times (service)
    get all the events (puntuals and recurrents) of a specific day and
    check the time-spaces where there's not any event and return those
    free time ranges in an array.
    
    getRecurrentsInDates as service: get the recurrent events and
    format it in Date objects to compare it with puntual events

--- src/user/controllers/getAvailables.js
+++ src/user/controllers/getAvailables.js
@@ -6,16 +6,7 @@ const getAvailableTimes = require(path.resolve(
 
 module.exports = async (req, res) => {
     try {
-        // res.status(200).send(await getAvailableTimes(new Date(req.query.date)))
-
-        // debug
-        res.status(200).send(
-            (await getAvailableTimes(new Date(req.query.date))).map(event => {
-                let start = event.start.toString()
-                let end = event.end.toString()
-                return { start, end }
-            })
-        )
+        res.status(200).send(await getAvailableTimes(new Date(req.query.date)))
     } catch (err) {
         res.status(500).send()
     }
